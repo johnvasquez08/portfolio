f26d67ff62229478f08ed8cdc68f0a10895e347a
johnvasquez08 <johndy0801939@gmail.com>
2024-11-06 16:53:45 -0500

    Portfolio act

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <h1  className="text-4xl w-full text-center">pf</h1>
+      <Header
+      />
     </>
   )
 }
